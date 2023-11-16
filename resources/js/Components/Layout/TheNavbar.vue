2e90ef8abffa493f6964051b3cb1ff46f1638a44
Stefano Bozzi <bozzistef@gmail.com>
2023-11-16 05:18:46 -0300

    refactor: logo component

--- resources/js/Components/Layout/TheNavbar.vue
+++ resources/js/Components/Layout/TheNavbar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import TheLogo from '@/Components/Layout/TheLogo.vue';
 
 import NavLink from '@/Components/NavLink.vue';
 import Dropdown from '@/Components/Dropdown.vue';
@@ -30,7 +30,7 @@ const showingNavigationDropdown = ref(false);
 					<!-- Logo -->
 					<div class="shrink-0 flex items-center">
 						<Link :href="route('dashboard')">
-						<ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+						<TheLogo class="block h-9 w-auto fill-current text-gray-800" />
 						</Link>
 					</div>
 
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,12 +1,12 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import TheLogo from '@/Components/Layout/TheLogo.vue';
 </script>
 
 <template>
 	<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
 		<div>
 			<Link href="/">
-			<ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+			<TheLogo class="w-20 h-20 fill-current text-gray-500" />
 			</Link>
 		</div>
 
