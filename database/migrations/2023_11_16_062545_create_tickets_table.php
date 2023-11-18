d2f850442478369a27b270ee22995ec566f1272a
Stefano Bozzi <bozzistef@gmail.com>
2023-11-18 03:31:41 -0300

    feat: define tickets migration

--- database/migrations/2023_11_16_062545_create_tickets_table.php
+++ database/migrations/2023_11_16_062545_create_tickets_table.php
@@ -6,22 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('tickets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('tickets', function (Blueprint $table) {
+			$table->id();
+			$table->string('title', 50);
+			$table->text('description', 1000);
+			$table->json('file_paths')->nullable();
+			$table->enum('status', ['open', 'closed'])->default('open');
+			$table->enum('priority', ['low', 'medium', 'high'])->default('low');
+			$table->unsignedBigInteger('assigned_user_id')->nullable(); // Foreign key to users table
+			$table->foreign('assigned_user_id')->references('id')->on('users');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('tickets');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('tickets');
+	}
 };
