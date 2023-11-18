39c5932e7623efea2c9d2a718e07533301948bf4
Stefano Bozzi <bozzistef@gmail.com>
2023-11-18 03:44:00 -0300

    feat: define model relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public function tickets(): BelongsToMany
+	{
+		return $this->belongsToMany(Ticket::class, 'category_ticket', 'category_id', 'ticket_id');
+	}
 }
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ticket extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public function users(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function labels(): BelongsToMany
+	{
+		return $this->belongsToMany(Label::class, 'ticket_label', 'ticket_id', 'label_id');
+	}
 }
