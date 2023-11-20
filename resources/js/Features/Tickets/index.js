2596c5d668510aa94799a1f6d8025705a4391c8b
Stefano Bozzi <bozzistef@gmail.com>
2023-11-20 07:06:31 -0300

    feat: create tickets table

---
+++ resources/js/Features/Tickets/index.js
@@ -0,0 +1,1 @@
+export { default as TicketsTable } from "./Components/TicketsTable.vue"
--- resources/js/Pages/Tickets/Index.vue
+++ resources/js/Pages/Tickets/Index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { TicketsTable } from "@/Features/Tickets";
 </script>
 
 <template>
@@ -6,15 +7,14 @@
 
 	<AuthenticatedLayout>
 		<template #header>
-			<h2 class="font-semibold text-xl text-gray-800 leading-tight">Tickets</h2>
+			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+				<h2 class="p-6 font-semibold text-xl text-gray-800 leading-tight">Tickets</h2>
+			</div>
 		</template>
 
-		<div class="py-12">
-			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-				<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-					<div class="p-6 text-gray-900">Tickets list</div>
-				</div>
-			</div>
-		</div>
+
+		<template #default>
+			<TicketsTable />
+		</template>
 	</AuthenticatedLayout>
 </template>
