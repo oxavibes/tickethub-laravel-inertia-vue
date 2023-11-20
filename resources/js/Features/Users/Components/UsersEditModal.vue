2cb8c236392db524bc024b9b4043b776112bd708
Stefano Bozzi <bozzistef@gmail.com>
2023-11-20 07:06:17 -0300

    refactor: users table

--- resources/js/Features/Users/Components/UsersEditModal.vue
+++ resources/js/Features/Users/Components/UsersEditModal.vue
@@ -3,7 +3,6 @@
 </script>
 
 <template>
-	<!-- Edit user modal -->
 	<div id="editUserModal" tabindex="-1" aria-hidden="true"
 		class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
 
--- resources/js/Features/Users/Components/UsersTable.vue
+++ resources/js/Features/Users/Components/UsersTable.vue
@@ -2,7 +2,7 @@
 import { onMounted } from "vue";
 import { initModals, initDropdowns } from "flowbite";
 
-import UsersTablePagination from "./UsersTablePagination.vue";
+import BasePagination from '@/Components/Shared/BasePagination.vue';
 
 onMounted(() => {
 	initModals()
@@ -12,18 +12,18 @@ onMounted(() => {
 
 <template>
 	<div class="relative bg-white overflow-x-auto shadow-sm sm:rounded-lg px-6 pt-4 pb-6">
-		<div class="flex justify-end py-4 bg-white ">
-			<label for="table-search" class="sr-only">Search</label>
-			<div class="relative">
+		<div class="pb-4 bg-white dark:bg-gray-900">
+			<label for="users-table-search" class="sr-only">Search</label>
+			<div class="relative mt-1">
 				<div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-					<svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-						viewBox="0 0 20 20">
+					<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+						fill="none" viewBox="0 0 20 20">
 						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
 							d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
 					</svg>
 				</div>
-				<input type="text" id="table-search-users"
-					class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+				<input type="text" id="users-table-search"
+					class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					placeholder="Search for users">
 			</div>
 		</div>
@@ -50,7 +50,7 @@ onMounted(() => {
 				<tr v-for="i in 10" class="bg-white border-b hover:bg-gray-50">
 					<th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
 						<div>
-							<span class="text-base font-semibold">Neil Sims</span>
+							<span class="font-medium">Neil Sims</span>
 						</div>
 					</th>
 					<td class="px-6 py-4">
@@ -59,7 +59,7 @@ onMounted(() => {
 					<td class="px-6 py-4">
 						<span class="font-normal text-gray-500">Agent</span>
 					</td>
-					<td class="px-6 py-4 space-x-2">
+					<td class="px-6 py-4 space-x-4">
 						<!-- Modal toggle -->
 						<a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal"
 							class="font-medium text-gray-900 hover:underline">Edit </a>
@@ -71,7 +71,7 @@ onMounted(() => {
 			</tbody>
 		</table>
 
-		<UsersTablePagination />
+		<BasePagination />
 	</div>
 </template>
 
