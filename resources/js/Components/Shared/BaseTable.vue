f7c3c7e9a6d92563b06a6241a53ec1b66bd198e4
Stefano Bozzi <bozzistef@gmail.com>
2023-11-27 05:05:16 -0300

    feat: update base table component

--- resources/js/Components/Shared/BaseTable.vue
+++ resources/js/Components/Shared/BaseTable.vue
@@ -1,16 +1,20 @@
 <script setup>
+import PrimaryButton from "@/Components/Buttons/PrimaryButton.vue";
 import BasePagination from '@/Components/Shared/BasePagination.vue';
 
-defineProps({
+const props = defineProps({
 	name: [String],
-	dataModalTargetEdit: [String],
-	dataModalTargetDelete: [String]
+	targetModalId: [String],
 })
+
+const dataModalTargetNew = `create${props.targetModalId}Modal`;
+const dataModalTargetEdit = `edit${props.targetModalId}Modal`;
+const dataModalTargetDelete = `delete${props.targetModalId}Modal`;
 </script>
 
 <template>
 	<div class="bg-white relative overflow-x-auto shadow-sm sm:rounded-lg px-6 pt-4 pb-6">
-		<div class="pb-4 bg-white dark:bg-gray-900">
+		<div class="pb-4 bg-white dark:bg-gray-900 flex flex-wrap gap-4 justify-between items-baseline">
 			<label :for="`${name}-table-search`" class="sr-only">Search</label>
 			<div class="relative mt-1">
 				<div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -24,6 +28,11 @@ defineProps({
 					class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					:placeholder="`Search for ${name}`">
 			</div>
+
+
+			<PrimaryButton :data-modal-target="dataModalTargetNew" :data-modal-show="dataModalTargetNew" class="h-[38px]">
+				<slot name="buttonText"></slot>
+			</PrimaryButton>
 		</div>
 
 		<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -58,8 +67,7 @@ defineProps({
 						True
 					</td>
 
-					<td class="px-6 py-4 space-x-4">
-						<!-- Modal toggle -->
+					<td class="px-6 py-4 flex flex-wrap gap-4">
 						<a href="#" type="button" :data-modal-target="dataModalTargetEdit" :data-modal-show="dataModalTargetEdit"
 							class="font-medium text-gray-900 hover:underline">Edit </a>
 
