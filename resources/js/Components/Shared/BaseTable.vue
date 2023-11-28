7b4779d8d7a60fd2435ce46b887fcc832d8c3147
Stefano Bozzi <bozzistef@gmail.com>
2023-11-28 06:29:33 -0300

    feat: update base table

--- resources/js/Components/Shared/BaseTable.vue
+++ resources/js/Components/Shared/BaseTable.vue
@@ -3,14 +3,13 @@ import PrimaryButton from "@/Components/Buttons/PrimaryButton.vue";
 import BasePagination from '@/Components/Shared/BasePagination.vue';
 
 const props = defineProps({
-	name: {
-		type: [String],
-		required: true,
-	},
 	headers: {
 		type: [Object],
 		required: true,
 		default: () => []
+	},
+	placeholder: {
+		type: [String],
 	},
 	data: {
 		type: [Array],
@@ -31,7 +30,7 @@ const dataModalTargetDelete = `delete${props.targetModalId}Modal`;
 <template>
 	<div class="bg-white relative overflow-x-auto shadow-sm sm:rounded-lg px-6 pt-4 pb-6">
 		<div class="pb-4 bg-white dark:bg-gray-900 flex flex-wrap gap-4 justify-between items-baseline">
-			<label :for="`${name}-table-search`" class="sr-only">Search</label>
+			<label :for="`${targetModalId}-table-search`" class="sr-only">Search</label>
 			<div class="relative mt-1">
 				<div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
 					<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -40,9 +39,9 @@ const dataModalTargetDelete = `delete${props.targetModalId}Modal`;
 							d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
 					</svg>
 				</div>
-				<input type="text" :id="`${name}-table-search`"
+				<input type="text" :id="`${targetModalId}-table-search`"
 					class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					:placeholder="`Search for ${name}`">
+					:placeholder="placeholder">
 			</div>
 
 
