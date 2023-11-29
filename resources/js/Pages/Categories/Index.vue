236323ec0120a4601afe6f2bbc41b89a0f6d3a7f
Stefano Bozzi <bozzistef@gmail.com>
2023-11-29 01:42:15 -0300

    feat: refactor categories page

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BaseTable from "@/Components/Shared/BaseTable.vue";
+import { CreateCategoryModal, DeleteCategoryModal, EditCategoryModal } from "@/Features/Categories";
 
 const headers = [
 	{
@@ -29,7 +30,6 @@ const headers = [
 			<h2 class="p-6 font-semibold text-xl text-gray-800 leading-tight">Categories</h2>
 		</template>
 
-
 		<template #default>
 			<BaseTable :headers="headers" targetModalId="Category" placeholder="Search for categories">
 				<template #button>
@@ -38,4 +38,8 @@ const headers = [
 			</BaseTable>
 		</template>
 	</AuthenticatedLayout>
+
+	<CreateCategoryModal />
+	<EditCategoryModal />
+	<DeleteCategoryModal />
 </template>
