4e83771fee39c944e5a740cc6a06f44a5b85f2f6
Stefano Bozzi <bozzistef@gmail.com>
2023-11-29 02:15:47 -0300

    feat: create category modals

---
+++ resources/js/Features/Categories/Components/DeleteCategoryModal.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BaseConfirmationModal from '@/Components/Shared/BaseConfirmationModal.vue';
+</script>
+
+<template>
+	<BaseConfirmationModal id="deleteCategoryModal">
+		<template #title>
+			Are you sure you want to delete this category?
+		</template>
+	</BaseConfirmationModal>
+</template>
+
+<style scoped>
+</style>
---
+++ resources/js/Features/Categories/index.js
@@ -0,0 +1,3 @@
+export { default as CreateCategoryModal } from "./Components/CreateCategoryModal.vue"
+export { default as EditCategoryModal } from "./Components/EditCategoryModal.vue"
+export { default as DeleteCategoryModal } from "./Components/DeleteCategoryModal.vue"
