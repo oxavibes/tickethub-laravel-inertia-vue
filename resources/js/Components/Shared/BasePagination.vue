bdc01b220f4cc8a29e4917c05b2d2c17a21844a3
Stefano Bozzi <bozzistef@gmail.com>
2023-12-14 03:28:43 -0300

    feat: improve base pagination

--- resources/js/Components/Shared/BasePagination.vue
+++ resources/js/Components/Shared/BasePagination.vue
@@ -1,44 +1,72 @@
 <script setup>
+import { computed } from "vue";
+defineProps({
+	data: {
+		type: [Object],
+		required: true,
+		default: () => { }
+	}
+})
+
+const computedComponent = computed(() => {
+	return (link) => {
+		if (link.url) {
+			return 'Link'
+		} else {
+			return 'span'
+		}
+	}
+});
+
+const computedLinkClass = computed(() => {
+	return (link) => {
+		if (link.label.includes('Previous')) {
+			return 'previous'
+		} else if (link.label.includes('Next')) {
+			return 'next'
+		} else if (link.active) {
+			return 'current'
+		} else {
+			return 'normal'
+		}
+	}
+});
 </script>
 
 <template>
 	<nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-8" aria-label="Table navigation">
-		<span
-			class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing
-			<span class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span
-				class="font-semibold text-gray-900 dark:text-white">1000</span></span>
+		<span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+			Showing
+			<span class="font-semibold text-gray-900 dark:text-white">{{ data.from }}-{{ data.to }}</span> of
+			<span class="font-semibold text-gray-900 dark:text-white">{{ data.total }}</span>
+		</span>
 		<ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-			<li>
-				<a href="#"
-					class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-			</li>
-			<li>
-				<a href="#"
-					class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-			</li>
-			<li>
-				<a href="#"
-					class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-			</li>
-			<li>
-				<a href="#" aria-current="page"
-					class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-			</li>
-			<li>
-				<a href="#"
-					class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-			</li>
-			<li>
-				<a href="#"
-					class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-			</li>
-			<li>
-				<a href="#"
-					class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+			<li v-for="link in data.links" :key="link.label">
+				<Component :is="computedComponent(link)" v-html="link.label" :href="link.url" class="base"
+					:class="computedLinkClass(link)" :aria-current="link.active ? 'page' : undefined" />
 			</li>
 		</ul>
 	</nav>
 </template>
 
-<style scoped>
+<style lang="postcss" scoped>
+.base {
+	@apply flex items-center justify-center px-3 h-8 border border-gray-300
+}
+
+.previous {
+	@apply ms-0 leading-tight text-gray-500 bg-white rounded-s-lg hover:bg-gray-100 hover:text-gray-700
+}
+
+.normal {
+	@apply leading-tight text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700
+}
+
+.current {
+	@apply text-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-700
+}
+
+.next {
+	@apply leading-tight text-gray-500 bg-white rounded-e-lg hover:bg-gray-100 hover:text-gray-700
+}
 </style>
