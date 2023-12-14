f6e1886df2fd09bcedcfddbf3f6b5fa37f78c3c4
Stefano Bozzi <bozzistef@gmail.com>
2023-12-14 02:36:28 -0300

    build: install pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import '../css/app.css';
 import 'flowbite';
 
 import { createApp, h } from 'vue';
+import { createPinia } from 'pinia'
 import { createInertiaApp, Head, Link } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
@@ -13,12 +14,15 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+const pinia = createPinia()
+
 createInertiaApp({
 	title: (title) => `${title} - ${appName}`,
 	resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
 	setup({ el, App, props, plugin }) {
 		return createApp({ render: () => h(App, props) })
 			.use(plugin)
+			.use(pinia)
 			.use(ZiggyVue, Ziggy)
 			.component('Link', Link)
 			.component('Head', Head)
