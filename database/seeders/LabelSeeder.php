3f65d820ea439618902ff03c35fef68f8cbc5145
Stefano Bozzi <bozzistef@gmail.com>
2023-12-14 02:36:02 -0300

    feat: create label factory, seeder

--- database/seeders/LabelSeeder.php
+++ database/seeders/LabelSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Label;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class LabelSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		Label::factory(10)->create();
+	}
 }
