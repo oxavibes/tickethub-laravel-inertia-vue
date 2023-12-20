6749b36819d5e5df46155c1d71b7176d56b4eb26
Stefano Bozzi <bozzistef@gmail.com>
2023-12-20 16:02:02 -0300

    feat: use HasRoles trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-	use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens;
+	use HasFactory;
+	use	Notifiable;
+	use	HasRoles;
 
 	/**
 	 * The attributes that are mass assignable.
