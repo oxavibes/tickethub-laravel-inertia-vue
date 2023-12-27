5f48262f5f31c22eadb158e3db986b4dc6ea4a4c
Stefano Bozzi <bozzistef@gmail.com>
2023-12-27 00:48:34 -0300

    feat: improve base table

--- resources/js/Components/Shared/BaseTable.vue
+++ resources/js/Components/Shared/BaseTable.vue
@@ -38,7 +38,7 @@ const search = ref(props.filters?.search);
 const emit = defineEmits(['onCreate', 'onEdit', 'onDelete']);
 
 watchDebounced(search, (value) => {
-	router.get(props.route, { search: value }, {
+	router.get(route(props.route), { search: value }, {
 		replace: true,
 		preserveState: true,
 	});
@@ -49,6 +49,7 @@ watchDebounced(search, (value) => {
 	<div class="bg-white relative overflow-x-auto shadow-sm sm:rounded-lg px-6 pt-4 pb-6">
 		<div class="pb-4 bg-white dark:bg-gray-900 flex flex-wrap gap-4 justify-between items-baseline">
 			<label :for="`${tableId}-table-search`" class="sr-only">Search</label>
+
 			<div class="relative mt-1">
 				<div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
 					<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -57,8 +58,9 @@ watchDebounced(search, (value) => {
 							d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
 					</svg>
 				</div>
+
 				<input type="text" :id="`${tableId}-table-search`" v-model="search"
-					class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
 					:placeholder="placeholder">
 			</div>
 
@@ -68,8 +70,8 @@ watchDebounced(search, (value) => {
 			</BaseButton>
 		</div>
 
-		<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+		<table class="w-full text-sm text-left rtl:text-right text-gray-500">
+			<thead class="text-xs text-gray-700 uppercase bg-gray-50">
 				<tr>
 					<th v-for="{ label, key } in headers" :key="key" scope="col" class="px-6 py-3">
 						{{ label }}
@@ -84,21 +86,14 @@ watchDebounced(search, (value) => {
 					</td>
 				</tr>
 
-				<tr v-for="record in data?.data" :key="record"
-					class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+				<tr v-for="record in data?.data" :key="record" class="bg-white border-b hover:bg-gray-50">
+					<template v-for="header in headers" :key="header.key">
+						<td v-if="!header.key.includes('actions')" class="px-6 py-4 text-gray-900">
+							{{ record[header.key] }}
+						</td>
+					</template>
 
-					<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-						{{ record.title }}
-					</th>
-
-					<td class="px-6 py-4">
-						{{ record.slug }}
-					</td>
-
-					<td class="px-6 py-4">
-						{{ record.visible }}
-					</td>
-
+					<!-- Actions -->
 					<td class="px-6 py-4 flex flex-wrap gap-4">
 						<a href="#" type="button" @click="$emit('onEdit', record)"
 							class="font-medium text-gray-900 hover:underline">Edit
