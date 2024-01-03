3307f600193fd4e34906c6cf5da01b22492f7ff9
Stefano Bozzi <bozzistef@gmail.com>
2024-01-03 14:46:56 -0300

    feat: create permission middleware

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermission
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+	 */
+	public function handle(Request $request, Closure $next, $permission)
+	{
+		if (!auth()->user()->hasPermissionTo($permission)) {
+			// Redirect or return an error response
+			return redirect()->back()->with('message', 'Unauthorized access');
+		}
+
+		return $next($request);
+	}
+}
