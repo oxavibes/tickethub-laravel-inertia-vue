bc937c3a200d33a3ae5fe7d9603f408a59bd3ec6
Stefano Bozzi <bozzistef@gmail.com>
2024-01-26 20:17:27 -0300

    build: install vue devtools

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
 	plugins: [
+		VueDevTools(),
 		laravel({
 			input: 'resources/js/app.js',
 			ssr: 'resources/js/ssr.js',
