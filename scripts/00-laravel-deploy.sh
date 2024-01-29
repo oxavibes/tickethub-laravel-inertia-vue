4b102daac762106b83fe25716134c899fc57fb66
Stefano Bozzi <bozzistef@gmail.com>
2024-01-29 01:05:21 -0300

    feat: update deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,14 +3,23 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
+echo "Autoloader Optimization..."
+composer install --optimize-autoloader --no-dev
+
+echo "Generating application key..."
 php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
 
+echo "Caching events..."
+php artisan event:cache
+
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Caching views..."
+php artisan view:cache
 
 echo "Running migrations..."
 php artisan migrate --seed --force
