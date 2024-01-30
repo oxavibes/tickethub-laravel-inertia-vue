13de7fbc439812779ff3076a635411df9bad2576
Stefano Bozzi <bozzistef@gmail.com>
2024-01-30 08:16:46 -0300

    feat: update base radio

--- resources/js/Components/Form/BaseRadio.vue
+++ resources/js/Components/Form/BaseRadio.vue
@@ -26,11 +26,13 @@ const computedModelValue = computed({
 
 <template>
 	<div class="flex-1">
-		<div class="flex items-center px-4 border border-gray-200 rounded">
-			<input v-model="computedModelValue" :id="option.id" type="radio" :value="option.value"
-				class="w-4 h-4 text-gray-800 bg-gray-100 border-gray-300 focus:ring-gray-800 focus:ring-2">
-			<label :for="option.id" class="w-full py-4 text-sm font-medium text-gray-900 ms-2">
-				{{ option.label }}
+		<div class="flex items-center">
+			<label :for="option.id"
+				class="w-full p-4 text-sm font-medium text-gray-900 border border-gray-200 rounded cursor-pointer has-[:checked]:outline has-[:checked]:outline-2">
+				<input v-model="computedModelValue" :id="option.id" type="radio" :value="option.value"
+					class="w-4 h-4 text-gray-800 bg-gray-100 border-gray-300 focus:ring-black checked:ring-gray-800 checked:ring-2">
+
+				<span class="ms-2">{{ option.label }}</span>
 			</label>
 		</div>
 	</div>
