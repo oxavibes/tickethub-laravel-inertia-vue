0ca8d2e19b7b70439006c5613c5194c3c015f440
Stefano Bozzi <bozzistef@gmail.com>
2024-02-02 07:07:23 -0300

    feat: improve auth layout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -5,13 +5,13 @@ import ToastList from '@/Components/Shared/ToastList.vue';
 
 <template>
 	<div>
-		<ToastList />
-
 		<div class="min-h-screen bg-gray-100">
+			<ToastList />
+
 			<TheNavbar />
 			<!-- Page Heading -->
 			<header v-if="$slots.header">
-				<div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+				<div class="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
 					<div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
 						<slot name="header" />
 					</div>
