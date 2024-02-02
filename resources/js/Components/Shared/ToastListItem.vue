3d0f7f2f28e5599afd8becb780a0ae39d2f5d3d9
Stefano Bozzi <bozzistef@gmail.com>
2024-02-02 07:07:38 -0300

    feat: improve toast components

--- resources/js/Components/Shared/ToastListItem.vue
+++ resources/js/Components/Shared/ToastListItem.vue
@@ -7,6 +7,7 @@ const props = defineProps({
 	},
 	type: {
 		type: [String],
+		default: '',
 	},
 	duration: {
 		type: Number,
@@ -22,8 +23,7 @@ onMounted(() => {
 </script>
 
 <template>
-	<div class="flex items-center p-4 text-gray-500 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-400"
-		role="alert">
+	<div class="flex items-center p-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
 		<div v-if="type === 'success'"
 			class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg">
 			<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -34,13 +34,14 @@ onMounted(() => {
 		</div>
 
 		<div v-else-if="type === 'error'"
-			class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+			class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
 			<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 				<path
 					d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
 			</svg>
 			<span class="sr-only">Error icon</span>
 		</div>
+
 
 		<div class="ml-3 text-sm font-normal">
 			{{ props.message }}
