b0d2038410bc1c0c7cfa07a93279945fd9c98cf2
Stefano Bozzi <bozzistef@gmail.com>
2024-02-02 02:05:58 -0300

    feat: create welcome page

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,11 +4,11 @@ import TheLogo from '@/Components/Layout/TheLogo.vue';
 
 <template>
 	<div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
-		<div>
+		<!-- <div>
 			<Link href="/">
 			<TheLogo class="w-20 h-20 text-gray-500 fill-current" />
 			</Link>
-		</div>
+		</div> -->
 
 		<div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
 			<slot />
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 		'laravelVersion' => Application::VERSION,
 		'phpVersion' => PHP_VERSION,
 	]);
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
 	return Inertia::render('Dashboard');
