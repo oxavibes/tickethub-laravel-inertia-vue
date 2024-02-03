7486d4635be8f35e053927cbbde422948123dbb4
Stefano Bozzi <bozzistef@gmail.com>
2024-02-03 06:28:57 -0300

    styles: update guestlayout

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheLogo from '@/Components/Layout/TheLogo.vue';
+// import TheLogo from '@/Components/Layout/TheLogo.vue';
 </script>
 
 <template>
-	<div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+	<div class="flex items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
 		<div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
 			<slot />
 		</div>
