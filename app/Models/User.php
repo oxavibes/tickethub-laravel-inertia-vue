ce4b5eaa97b56b3c90bd5360bb85cb467111835a
Stefano Bozzi <bozzistef@gmail.com>
2024-02-06 21:41:50 -0300

    feat: implement user must verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,12 +11,13 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
 	use HasApiTokens;
 	use HasFactory;
 	use	Notifiable;
 	use	HasRoles;
+	// use MustVerifyEmail;
 
 	/**
 	 * The attributes that are mass assignable.
