2db8addddce16100c6aa9b2beaf5478544ea6bbd
Stefano Bozzi <bozzistef@gmail.com>
2024-02-25 23:47:27 -0300

    build: update deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,20 +6,11 @@ composer install --no-dev --working-dir=/var/www/html
 echo "Autoloader Optimization..."
 composer install --optimize-autoloader --no-dev
 
-echo "Generating application key..."
-php artisan key:generate --show
-
 echo "Caching config..."
 php artisan config:cache
 
-echo "Caching events..."
-php artisan event:cache
-
 echo "Caching routes..."
 php artisan route:cache
-
-echo "Caching views..."
-php artisan view:cache
 
 echo "Running migrations..."
 php artisan migrate --seed --force
