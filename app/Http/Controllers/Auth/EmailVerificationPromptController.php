e334523413085c901411cf018a95c2c43ad5c820
Stefano Bozzi <bozzistef@gmail.com>
2024-03-03 01:40:42 -0300

    Revert "feat: redirect user based on role"
    This reverts commit b4e9395853c02dd77427b3cbcd225ef88b99d16c.

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -18,7 +17,7 @@ class EmailVerificationPromptController extends Controller
 	public function __invoke(Request $request): RedirectResponse|Response
 	{
 		return $request->user()->hasVerifiedEmail()
-			? redirect(Auth::user()->getRedirectRoute())
+			? redirect()->intended(RouteServiceProvider::HOME)
 			: Inertia::render('Auth/VerifyEmail', ['status' => session('status')]);
 	}
 }
