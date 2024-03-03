b4e9395853c02dd77427b3cbcd225ef88b99d16c
Stefano Bozzi <bozzistef@gmail.com>
2024-03-03 01:30:36 -0300

    feat: redirect user based on role

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -6,20 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EmailVerificationNotificationController extends Controller
 {
-    /**
-     * Send a new email verification notification.
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME);
-        }
+	/**
+	 * Send a new email verification notification.
+	 */
+	public function store(Request $request): RedirectResponse
+	{
+		if ($request->user()->hasVerifiedEmail()) {
+			redirect(Auth::user()->getRedirectRoute());
+		}
 
-        $request->user()->sendEmailVerificationNotification();
+		$request->user()->sendEmailVerificationNotification();
 
-        return back()->with('status', 'verification-link-sent');
-    }
+		return back()->with('status', 'verification-link-sent');
+	}
 }
--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -17,7 +18,7 @@ class EmailVerificationPromptController extends Controller
 	public function __invoke(Request $request): RedirectResponse|Response
 	{
 		return $request->user()->hasVerifiedEmail()
-			? redirect()->intended(RouteServiceProvider::HOME)
+			? redirect(Auth::user()->getRedirectRoute())
 			: Inertia::render('Auth/VerifyEmail', ['status' => session('status')]);
 	}
 }
